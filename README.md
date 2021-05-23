3d0e820b54f130579c4aae5b7509621b928df832
Jack Kilgore <kilgorej99@gmail.com>
2021-05-23 16:56:24 -0700

    named controls working and preparing for binaryopugen

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # synthdefjs
 SuperCollider SynthDef in Native Javascript
+
+## TODO
+
+UGen's that have a mul parameter don't actually pass this parameter to the server. The synthdef instead creates a BinOpUgen and multiplies the SinOsc with it. Thus your mul parameters will not work out of the box. Mul and add of Oscillators are just an alias for BinaryOpUGen. We finally have to deal with :((
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const {SynthDef} = require('./src/SynthDef')
 const {Out, SinOsc} = require('./src/UGen')
-const { insertPascalString } = require('./src/Utilities')
 
 
 let def0 = SynthDef("def0", () => {
@@ -8,14 +7,9 @@ let def0 = SynthDef("def0", () => {
     Out.ar(0, sig)
 })
 
-// Flaw: Default functions values are loaded into the synthDefFile as constants.
-// Same with the namedControl arguments. SC does not do this.
-
-// Fixed the named control part.
-
 // I think I will stick to the named argument style.
 let def1 = SynthDef("def1", () => {
-    let sig = SinOsc.ar("freq".kr(220), 0, "amp".kr(0.2))
+    let sig = SinOsc.ar("freq".kr(220), 0, 0.2)
     let a = Out.ar(0, sig)
 })
 
@@ -27,12 +21,10 @@ let def2 = SynthDef("def2", () => {
     let a = Out.ar("out".kr(0), sig)
 })
 */
+
 // If you load this file into SC, it works!
-// Notice that SC implicitly uses BinOp instead of the mul param.
-//  The sclang SinOsc only had 2 params: (freq, phase)
 //def0.writeToFile("/Users/jkilgore/Desktop/doi.scsyndef")
 
-// If you load this file into SC, it fails silently (literally).
-// TODO: DEBUG; The way we handle controls is flawed. 
+// If you load this file into SC, it fails silently (literally). 
 //console.log(def1.readableSynthDefFile())
-def1.writeToFile("/Users/jkilgore/Desktop/doi.scsyndef")
+def1.writeToFile("/path/to/scsyndef")
